30890557d1b8f2ea83c1cefbc797f25d4f2bb15f
M1-134-HamzaFaisal <76451177+M1-134-HamzaFaisal@users.noreply.github.com>
2021-01-07 20:40:16 +0300

    DONE

--- myscript.js
+++ myscript.js
@@ -3,12 +3,44 @@ let board = [
     ['10', '11', '12'],
     ['20', '21', '22']
 ]
+let turn ="o"
 
 
-
-function press(cellNO) {
-
-    // Do stuff
-    console.log(cellNO)
+function press(x,y) {
+     // Do stuff
+     console.log(x,y)
     
+    if (turn == "o"){
+        turn="x"
+    }else{
+        turn="o"
+    }
+    board[x][y]=turn
+    let cell = document.getElementById(`${x}${y}`);
+    cell.innerText =turn;
+    if(turn== board [0][0] && board[0][1]==turn && board[0][2]==turn){
+        alert(`goooooooooooooood${turn}`)
+    }else if(turn==board [1][0] && board[1][1]==turn &&board [1][2]==turn){
+        alert(`goooooooooooooood${turn}`)
+    }else if(turn==board [2][0] && board[2][1]==turn &&board [2][2]==turn){
+        alert(`goooooooooooooood${turn}`)
+    }else if(turn==board [0][0] && board[1][0]==turn && board[2][0]==turn){
+        alert(`goooooooooooooood${turn}`)
+    }else if(turn==board [0][1]&& board[1][1]==turn && board[2][1]==turn){
+        alert(`goooooooooooooood${turn}`)
+    }else if(turn==board [0][2] && board[1][2]==turn &&board [2][2]==turn){
+        alert(`goooooooooooooood${turn}`)
+    }else if(turn==board [0][0] && board[1][1]==turn && board[2][2]==turn){
+        alert(`goooooooooooooood${turn}`)
+    }else if (turn==board [0][2] && board[1][1]==turn &&board [2][0]==turn){
+        alert(`goooooooooooooood${turn}`)
+    }
+
+
+
+
+
+
+
+   
 }
